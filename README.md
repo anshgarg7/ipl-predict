04ce97928c3d5c0f7882ce3f70f9600d5a304390
Ansh Garg <anshgarg7@gmail.com>
2021-10-16 16:34:18 +0530

    project updated

--- README.md
+++ README.md
@@ -1,52 +1,55 @@
+# IPL Score Predictor :india:
+![GIF](readme_resources/ipl_teams.jpg)
 
-<br />
-<p align="center">
-  <a href="https://github.com/HarshCasper/IPL-Match-Predictor">
-    <img src="https://vaastuyogam.com/wp-content/uploads/2017/05/Indian_Premier_League.png" alt="Logo" width="80" height="80">
-  </a>
+#### The IPL Score Predictor project is about predicting score of batting team.
+#### This web application have IPL 2021 teams where you can predict score of batting team.
 
-  <h3 align="center">IPL Match Predictor</h3>
+## Overview
+- I have IPL matches records from year 2008 to 2020.
+- I have trained machine learning model for 8 teams which were playing in year 2021.
+- Here i have performed hyperparameter tunning on various model and i got **Random Forest** as best model for this problem statement.
+- Gradient Booost algorithm gives **0.850539 r2_score** & **1.632532 MAE** on train dataset and **0.871335 r2_score** & **6.438084 MAE** on test dataset.
+- Random Forest algorithm gives **0.672894 r2_score** & **11.296707 MAE** on train dataset and **0.681484 r2_score** & **12.173585 MAE** on test dataset.
+- Difference of train dataset MAE and test dataset MAE is **4.805552** for Gradient Boost.
+- Difference of train dataset MAE and test dataset MAE is **0.876878** for Random Forest.
+- Here accuracy or **r2_score** of Gradient Boost algorithm is higher than Random Forest but Random Forest's **MAE** difference between train and test dataset is lower than Gradient Boost.
+- If i select Gradient Boost algorithm which has high r2_score then our model will **not perform stable** as it performed on train dataset because **MAE** difference between train and test dataset is **high** that's why it might generate **overfitting** issue.
+- If i select Radom Forest algorithm which does not have high r2_score still it will perfrom **stable** as it performed on train dataset because MAE difference between train and test dataset is **low**.
+- Here i have selected **Random Forest** algorithm with it's hyperparamter which best fit for this problem statement with around **68%** of accuracy.
+- Application developed using Flask, Javascript, Bootstrap, CSS and HTML
 
-  <p align="center">
-    A simple Machine Learning Application developed and deployed on Flask Web Framework to predict an IPL Match Winner 🏏🎰
-    <br />
-    
-  </p>
-</p>
+## Data Source
+- In this project i have used IPL dataset from kaggle, you can get it from [here](https://www.kaggle.com/patrickb1912/ipl-complete-dataset-20082020)
 
-## About The Project
+## Demo
+- I have deployed this on Heroku platform
+Link: [https://ipl-jaysoftic.herokuapp.com/](https://ipl-jaysoftic.herokuapp.com/)
 
-IPL Match Predictor is a Flask Application that deploys a Machine Learning Model that can predict the winner of an IPL Match. The Algorithm
-used to develop and deploy the Machine Learning Application was Random Forest and has been developed to research intuitive work on utilizing Machine Learning in Sports and Academics. The project was displayed during Project Expo held on February 1, 2020 at Sathyabama Institute of Science and Technology.
+![GIF](readme_resources/IPL.gif)
 
-## Technology Stack
+## How to use
+- Select venue, batting team, bowling tean and fill the inputs with proper information then click on predict button you will get predicted score of batting team.
 
-* [Flask](https://github.com/pallets/flask)
-* [HTML](https://www.w3.org/TR/html52/)
-* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
-* [Bootstrap](https://getbootstrap.com/)
+## Technologies Used
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"></code>
+<code><img height="30" src="https://github.com/tomchen/stack-icons/raw/master/logos/bootstrap.svg"></code>
+<code><img height="30" src="https://symbols.getvecta.com/stencil_80/56_flask.3a79b5a056.jpg"></code>
+<code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/e/ec/Heroku_logo.svg"></code>
 
-## Local Installation
+<code><img height="30" src="https://raw.githubusercontent.com/numpy/numpy/7e7f4adab814b223f7f917369a72757cd28b10cb/branding/icons/numpylogo.svg"></code>
+<code><img height="30" src="https://matplotlib.org/_static/logo2.svg"></code>
+<code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/330px-Scikit_learn_logo_small.svg.png"></code>
 
-1. Drop a ⭐ on the Github Repository. 
-2. Clone the Repo by going to your local Git Client and pushing in the command: 
+## Team
+[![Jay Soni](https://avatars3.githubusercontent.com/u/49163967?s=400&u=be22bbe1409ff51991b04026f038c1373174a02a&v=4)](https://in.linkedin.com/in/jaysoftic) |
+-|
+[Jay Soni](https://in.linkedin.com/in/jaysoftic) |)
 
-```sh
-git clone https://github.com/HarshCasper/IPL-Match-Predictor.git
-```
+## Credits
+- Entire credits goes to My God
 
-3. Install the Packages: 
-```sh
-pip install -r requirements.txt
-```
-
-4. At last, push in the command:
-```sh
-python app.py
-```
-
-5. Go to ` http://127.0.0.1:5000/` and enjoy the application.
-
-## LICENSE
-
-[MIT](https://github.com/HarshCasper/IPL-Match-Predictor/blob/master/LICENSE)
+## 
+- If you like my work and it helped you in anyway then please do ⭐ the repository it will motivate me to make more amazing projects
--- app.py
+++ app.py
@@ -1,93 +1,35 @@
-import flask
-import pickle
-import pandas as pd
-import numpy as np
+from flask import Flask, request, render_template
+import util
 
-with open(f'model/ipl_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+app = Flask(__name__)
 
-app = flask.Flask(__name__, template_folder='templates')
+teams, venues = util.load_artifacts()
+teams.sort()
+venues.sort()
 
-@app.route('/', methods=["GET", "POST"])
-def main():
-    if flask.request.method == 'GET':
-        return(flask.render_template('main.html'))
+@app.route("/")
+def home():
+    return render_template("home.html", team = teams, venue = venues)
 
-    if flask.request.method == 'POST':
-        city = flask.request.form['city']
-        Home = flask.request.form['Home']
-        Away = flask.request.form['Away']
-        toss_winner = flask.request.form['toss_winner']
-        toss_decision = flask.request.form['toss_decision']
-        venue = flask.request.form['venue']
+@app.route("/predictedScore", methods = ["POST"])
+def predicted_score():
+    try:
+        venue = request.form["venue"]
+        bat_team = request.form["bat_team"]
+        bowl_team = request.form["bowl_team"]
+        overs = float(request.form["overs"])
+        runs = int(request.form["runs"])
+        wickets = int(request.form["wickets"])
+        runs_last_5 = int(request.form["runs_last_5"])
+        wickets_last_5 = int(request.form["wickets_last_5"])
 
-        if toss_winner == 'Home Team':
-            toss_winner = Home
+        result = int(util.predict_score(overs, wickets, runs, wickets_last_5, runs_last_5, bat_team, bowl_team, venue))
+        if result == 1:
+            return "Something is wrong please fill proper input!!"
         else:
-            toss_winner = Away
+            return "Predicted score range of " + bat_team + " is between " + str(result - 5) + " to " + str(result + 5)
+    except :
+        return "Something is wrong please fill proper input!!"
 
-        input_variables = pd.DataFrame([[city, Home, Away, toss_winner, toss_decision, venue]], columns=['city', 'Home', 'Away', 'toss_winner',
-        'toss_decision', 'venue'], dtype=object)
-
-        input_variables.Home.replace(['Sunrisers Hyderabad', 'Mumbai Indians', 'Gujarat Lions',
-                      'Rising Pune Supergiant', 'Royal Challengers Bangalore',
-                      'Kolkata Knight Riders', 'Delhi Capitals', 'Kings XI Punjab',
-                      'Chennai Super Kings', 'Rajasthan Royals', 'Deccan Chargers',
-                      'Kochi Tuskers Kerala', 'Pune Warriors', 'Rising Pune Supergiants'],
-                      np.arange(0, 14), inplace=True)
-        input_variables.Away.replace(['Sunrisers Hyderabad', 'Mumbai Indians', 'Gujarat Lions',
-                      'Rising Pune Supergiant', 'Royal Challengers Bangalore',
-                      'Kolkata Knight Riders', 'Delhi Capitals', 'Kings XI Punjab',
-                      'Chennai Super Kings', 'Rajasthan Royals', 'Deccan Chargers',
-                      'Kochi Tuskers Kerala', 'Pune Warriors', 'Rising Pune Supergiants'],
-                      np.arange(0, 14), inplace=True)
-        #input_variables['toss_winner'] = np.where(input_variables['toss_winner'] == 'Home Team', input_variables['Home'], input_variables['Away'])
-        input_variables.toss_winner.replace(['Sunrisers Hyderabad', 'Mumbai Indians', 'Gujarat Lions',
-                             'Rising Pune Supergiant', 'Royal Challengers Bangalore',
-                             'Kolkata Knight Riders', 'Delhi Capitals', 'Kings XI Punjab',
-                             'Chennai Super Kings', 'Rajasthan Royals', 'Deccan Chargers',
-                             'Kochi Tuskers Kerala', 'Pune Warriors', 'Rising Pune Supergiants'],
-                              np.arange(0, 14), inplace=True)
-        input_variables.toss_decision.replace(['bat', 'field'], [0, 1], inplace=True)
-        input_variables.city.replace(['Hyderabad', 'Pune', 'Rajkot', 'Indore', 'Bangalore', 'Mumbai',
-        'Kolkata', 'Delhi', 'Chandigarh', 'Kanpur', 'Jaipur', 'Chennai',
-        'Cape Town', 'Port Elizabeth', 'Durban', 'Centurion',
-        'East London', 'Johannesburg', 'Kimberley', 'Bloemfontein',
-        'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala', 'Kochi',
-        'Visakhapatnam', 'Raipur', 'Ranchi', 'Abu Dhabi', 'Sharjah'],
-        np.arange(0, 30), inplace=True)
-        input_variables.venue.replace(['Rajiv Gandhi International Stadium, Uppal',
-        'Maharashtra Cricket Association Stadium',
-        'Saurashtra Cricket Association Stadium', 'Holkar Cricket Stadium',
-        'M Chinnaswamy Stadium', 'Wankhede Stadium', 'Eden Gardens',
-        'Feroz Shah Kotla',
-        'Punjab Cricket Association IS Bindra Stadium, Mohali',
-        'Green Park', 'Punjab Cricket Association Stadium, Mohali',
-        'Sawai Mansingh Stadium', 'MA Chidambaram Stadium, Chepauk',
-        'Dr DY Patil Sports Academy', 'Newlands', "St George's Park",
-        'Kingsmead', 'SuperSport Park', 'Buffalo Park',
-        'New Wanderers Stadium', 'De Beers Diamond Oval',
-        'OUTsurance Oval', 'Brabourne Stadium',
-        'Sardar Patel Stadium, Motera', 'Barabati Stadium',
-        'Vidarbha Cricket Association Stadium, Jamtha',
-        'Himachal Pradesh Cricket Association Stadium', 'Nehru Stadium',
-        'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium',
-        'Subrata Roy Sahara Stadium',
-        'Shaheed Veer Narayan Singh International Stadium',
-        'JSCA International Stadium Complex', 'Sheikh Zayed Stadium',
-        'Sharjah Cricket Stadium'],
-        np.arange(0, 34), inplace=True)
-        prediction = model.predict(input_variables)
-        prediction = pd.DataFrame(prediction, columns=['Winners'])
-        prediction = prediction["Winners"].map({0:'Sunrisers Hyderabad', 1:'Mumbai Indians', 2:'Gujarat Lions',
-                      3:'Rising Pune Supergiant', 4:'Royal Challengers Bangalore',
-                      5:'Kolkata Knight Riders', 6:'Delhi Capitals', 7:'Kings XI Punjab',
-                      8:'Chennai Super Kings', 9:'Rajasthan Royals', 10:'Deccan Chargers',
-                      11:'Kochi Tuskers Kerala', 12:'Pune Warriors', 13:'Rising Pune Supergiants'})
-        return flask.render_template('main.html', original_input={'city':city, 'Home':Home, 'Away':Away, 'toss_winner':toss_winner, 'toss_decision':toss_decision,
-                                     'venue':venue},
-                                    result=prediction[0],
-                                    )
-
-if __name__=='__main__':
-    app.run(debug="True")
+if __name__ == "__main__":
+    app.run()
